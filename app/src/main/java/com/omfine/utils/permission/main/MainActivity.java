74a3310c16f21bc54d8a11b43ad4ae24b22c8b01
shengshi <eijiyey@163.com>
2021-09-01 17:09:12 +0800

    demo

--- app/src/main/java/com/omfine/utils/permission/main/MainActivity.java
+++ app/src/main/java/com/omfine/utils/permission/main/MainActivity.java
@@ -1,8 +1,15 @@
 package com.omfine.utils.permission.main;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.omfine.utils.permission.ext.OnPermissionRequestResultListener;
+import com.omfine.utils.permission.ext.PermissionHelper;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.permission).setOnClickListener(view -> {
 
+            PermissionHelper.requestPermission(this , Manifest.permission.WRITE_EXTERNAL_STORAGE , new OnPermissionRequestResultListener(){
+
+                @Override
+                public void onDenied(ArrayList<String> grantedPermissions, ArrayList<String> deniedWithRemindPermissions, ArrayList<String> deniedWithoutRemindPermissions) {
+                    Log.e("permission" , "=======onDenied====00=====" + deniedWithoutRemindPermissions.size());
+                }
+                @Override
+                public void onGranted() {
+
+                }
+            });
 
         });
 
